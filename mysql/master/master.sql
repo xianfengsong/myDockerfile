80b6c13b9f8bdcf00a542eb3aa291c3c43b4b18f
xianfengsong <songx@q.com>
2018-10-10 19:50:44 +0800

    更新初始sql,之前文件太大上传不了

--- mysql/master/master.sql
+++ mysql/master/master.sql
@@ -1,3 +1,93 @@
+-- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: shop
+-- ------------------------------------------------------
+-- Server version	5.6.41-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `shop`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `shop` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `shop`;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `amount` bigint(20) DEFAULT '0' COMMENT '库存',
+  `name` varchar(60) DEFAULT NULL COMMENT '名称',
+  `description` varchar(2048) DEFAULT NULL COMMENT '描述',
+  `category` varchar(40) DEFAULT NULL COMMENT '类目',
+  `price` int(20) unsigned DEFAULT NULL COMMENT '价格 小数X100',
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `index_name` (`price`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,10,'Iphone','bigger than bigger','phone',8999,'2018-09-27 09:27:01'),(2,1000000,'Xiaomi','born for hot','phone',1999,'2018-09-27 09:27:01'),(7,1,'test','desc','phone',10000,'2018-09-27 00:00:00'),(8,1,'test','desc','phone',10000,'2018-09-27 00:00:00'),(9,1,'test','desc','phone',10000,'2018-09-27 20:55:50'),(10,1,'test','desc','phone',10000,'2018-09-27 21:11:58'),(11,1,'test','desc','phone',10000,'2018-09-27 21:12:25'),(12,1,'test2','desc','phone',10000,'2018-09-27 21:17:45'),(13,1,'testX','desc','phone',10000,'2018-09-28 15:46:16');
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item_order`
+--
+
+DROP TABLE IF EXISTS `item_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item_order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `item_id` bigint(20) NOT NULL COMMENT '商品id',
+  `user_id` varchar(40) DEFAULT NULL COMMENT '用户id uuid',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item_order`
+--
+
+LOCK TABLES `item_order` WRITE;
+/*!40000 ALTER TABLE `item_order` DISABLE KEYS */;
+INSERT INTO `item_order` VALUES (1,1,'Tom');
+/*!40000 ALTER TABLE `item_order` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-10 19:49:48
 -- 创建主从复制账号
 CREATE USER 'repl'@'%' IDENTIFIED BY 'repl';
 GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';
